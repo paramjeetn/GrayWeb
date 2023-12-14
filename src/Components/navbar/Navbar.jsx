30521d7c30f98ff957e0e5e56a74fbca302be43a
Paramjeet <paramjeetnpradhan@gmail.com>
2023-12-14 22:15:15 +0530

    dark mode  error resolved

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,8 +1,8 @@
-import "./navbar.scss";
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
-import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
@@ -10,29 +10,33 @@ import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { DarkModeContext } from "../../context/darkModeContext";
+import "./navbar.scss";
 
 const Navbar = () => {
+  const { toggle, darkMode } = useContext(DarkModeContext);
 
-  const {toggle,darkMode}=useContext(DarkModeContext);
-
-   <div className="navbar">
-    <div className="left">
-      <Link to="/" style={{textDecoration:"none"}}>
-      <span>GrayWeb</span>
-      </Link>
-      <HomeOutlinedIcon />
-     { darkMode ? <WbSunnyOutlinedIcon onClick={toggle} /> :<DarkModeOutlinedIcon onClick={toggle}/>}
-      <GridViewOutlinedIcon />
-      <div className="search">
-        <SearchOutlinedIcon />
-        <input type="text" placeholder="Search" />
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>GrayWeb</span>
+        </Link>
+        <HomeOutlinedIcon />
+        {darkMode ? (
+          <WbSunnyOutlinedIcon onClick={toggle} style={{cursor: "pointer"}}/>
+        ) : (
+          <DarkModeOutlinedIcon onClick={toggle} style={{cursor:"pointer"}} />
+        )}
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search..." />
+        </div>
       </div>
-      
-    </div>
-    <div className="right">
-      <PersonOutlinedIcon />
-      <EmailOutlinedIcon />
-      <NotificationsOutlinedIcon />
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
       <div className="user">
         <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=""/>
         <span>John Doe</span>
@@ -40,6 +44,6 @@ const Navbar = () => {
     </div>
    </div>
   
-}
+);};
 
 export default Navbar
