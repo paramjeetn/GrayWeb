5f13cbca4e08388624ca24ac1c5b315f63ea1b8f
Paramjeet <paramjeetnpradhan@gmail.com>
2023-12-14 22:24:25 +0530

    input box outline removed

--- src/Components/leftBar/LeftBar.jsx
+++ src/Components/leftBar/LeftBar.jsx
@@ -19,7 +19,7 @@ const LeftBar = () => {
       <div className="container">
         <div className="menu">
           <div className="user">
-          {/* <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=""/> */}
+          <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=""/>
         <span>John Doe</span>
           </div>
           <div className="item">
